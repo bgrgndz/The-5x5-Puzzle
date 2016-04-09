1065ba7ada721acfd8d5b73ac8d9ffe2cba64d5e
Bugra Gunduz <bugragndz@gmail.com>
2016-04-09 21:07:13 +0300

    debug

--- js/main.js
+++ js/main.js
@@ -6,73 +6,63 @@ window.onload = function() {
 							 "5,1","5,2","5,3","5,4","5,5"];
 	var index = 1;
 	var taken = [];
-	var ex = $();
 	$(".gridcell").on("click",function() {
 		if(this.innerHTML === "" && this.className !== "noclick"){
-			$("#undo").on("click",function(){
-				main(ex);
-			});
-				main(this);
-				//Undo Script
+			var pos = this.className;
+			pos = pos.replace("gridcell ", "");
+			pos = pos.replace("row", "");
+			pos = pos.replace("col", "");
+			pos = pos.split(" ");
+			var col = parseInt(pos[0]);
+			var row = parseInt(pos[1]);
+			if(cango.indexOf(row.toString() + "," + col.toString()) !== -1){
+				$(".current").removeClass("current");
+				$(".cango").removeClass("cango");
+				this.innerHTML = index;
+				index = index + 1;
+				$(this).css("cursor","default");
+				$(this).addClass("noclick");
+				$(this).addClass("current");
+				taken[taken.length] = row + "," + col;
+				cango = [];
+				if(row - 3 > 0 && taken.indexOf((row - 3).toString() + "," + col) === -1){
+					cango[cango.length] = (row - 3).toString() + "," + col.toString();
+				}
+				if(row + 3 < 6 && taken.indexOf((row + 3).toString() + "," + col) === -1){
+					cango[cango.length] = (row + 3).toString() + "," + col.toString();
+				}
+				if(col - 3 > 0 && taken.indexOf(row + "," + (col - 3).toString()) === -1){
+					cango[cango.length] = row + "," + (col - 3).toString();
+				}
+				if(col + 3 < 6 && taken.indexOf(row + "," + (col + 3).toString()) === -1){
+					cango[cango.length] = row + "," + (col + 3).toString();
+				}
+				if(row - 2 > 0 && col - 2 > 0 && taken.indexOf((row - 2).toString() + "," + (col - 2).toString()) === -1){
+					cango[cango.length] = (row - 2).toString() + "," + (col - 2).toString();
+				}
+				if(row + 2 < 6 && col + 2 < 6 && taken.indexOf((row + 2).toString() + "," + (col + 2).toString()) === -1){
+					cango[cango.length] = (row + 2).toString() + "," + (col + 2).toString();
+				}
+				if(row - 2 > 0 && col + 2 < 6 && taken.indexOf((row - 2).toString() + "," + (col + 2).toString()) === -1){
+					cango[cango.length] = (row - 2).toString() + "," + (col + 2).toString();
+				}
+				if(row + 2 < 6 && col - 2 > 0 && taken.indexOf((row + 2).toString() + "," + (col - 2).toString()) === -1){
+					cango[cango.length] = (row + 2).toString() + "," + (col - 2).toString();
+				}
+				if(cango.length === 0){
+					$("#modal").modal();
+				}else{
+					var nowcango;
+					for(var i = 0; i < cango.length;i++){
+						nowcango = cango[i].split(",");
+						nowcango = ".row" + nowcango[0] + ".col" + nowcango[1];
+						$(nowcango).addClass("cango");
+					}
+				}
 			}
 		}
 	});
 	$(".btn").on("click",function(){
 		location.reload();
 	});
-}
-function main(obj){
-	var pos = obj.className;
-	pos = pos.replace("gridcell ", "");
-	pos = pos.replace("row", "");
-	pos = pos.replace("col", "");
-	pos = pos.split(" ");
-	var col = parseInt(pos[0]);
-	var row = parseInt(pos[1]);
-	if(cango.indexOf(row.toString() + "," + col.toString()) !== -1){
-		var ex = obj;
-		$(".current").removeClass("current");
-		$(".cango").removeClass("cango");
-		obj.innerHTML = index;
-		index = index + 1;
-		$(obj).css("cursor","default");
-		$(obj).addClass("noclick");
-		$(obj).addClass("current");
-		taken[taken.length] = row + "," + col;
-		cango = [];
-		if(row - 3 > 0 && taken.indexOf((row - 3).toString() + "," + col) === -1){
-			cango[cango.length] = (row - 3).toString() + "," + col.toString();
-		}
-		if(row + 3 < 6 && taken.indexOf((row + 3).toString() + "," + col) === -1){
-			cango[cango.length] = (row + 3).toString() + "," + col.toString();
-		}
-		if(col - 3 > 0 && taken.indexOf(row + "," + (col - 3).toString()) === -1){
-			cango[cango.length] = row + "," + (col - 3).toString();
-		}
-		if(col + 3 < 6 && taken.indexOf(row + "," + (col + 3).toString()) === -1){
-			cango[cango.length] = row + "," + (col + 3).toString();
-		}
-		if(row - 2 > 0 && col - 2 > 0 && taken.indexOf((row - 2).toString() + "," + (col - 2).toString()) === -1){
-			cango[cango.length] = (row - 2).toString() + "," + (col - 2).toString();
-		}
-		if(row + 2 < 6 && col + 2 < 6 && taken.indexOf((row + 2).toString() + "," + (col + 2).toString()) === -1){
-			cango[cango.length] = (row + 2).toString() + "," + (col + 2).toString();
-		}
-		if(row - 2 > 0 && col + 2 < 6 && taken.indexOf((row - 2).toString() + "," + (col + 2).toString()) === -1){
-			cango[cango.length] = (row - 2).toString() + "," + (col + 2).toString();
-		}
-		if(row + 2 < 6 && col - 2 > 0 && taken.indexOf((row + 2).toString() + "," + (col - 2).toString()) === -1){
-			cango[cango.length] = (row + 2).toString() + "," + (col - 2).toString();
-		}
-		if(cango.length === 0){
-			$("#modal").modal();
-		}else{
-			var nowcango;
-			for(var i = 0; i < cango.length;i++){
-				nowcango = cango[i].split(",");
-				nowcango = ".row" + nowcango[0] + ".col" + nowcango[1];
-				$(nowcango).addClass("cango");
-			}
-		}
-		return true;
 }
