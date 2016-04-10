6515eca97d8540e89b9ce4a51b7352fd21e789ce
Bugra Gunduz <bugragndz@gmail.com>
2016-04-10 11:22:53 +0300

    styling

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ window.onload = function() {
 							 "3,1","3,2","3,3","3,4","3,5",
 							 "4,1","4,2","4,3","4,4","4,5",
 							 "5,1","5,2","5,3","5,4","5,5"];
+	$(".gridcell").addClass("cango");
 	var index = 1;
 	var taken = [];
 	$(".gridcell").on("click",function() {
